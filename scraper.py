733138b8bf09bce5a408db086673bb1f9579ebdf
Warren Wei Leu <wwleu@circinus-42.ics.uci.edu>
2022-01-24 14:30:57 -0800

    Added Initial Crawling Function
    Current the crawler goes to the given seed links, gets the links from the webpages, and crawls to the new links.
    It remembers the links it has been to and does not add those links to the frontier again
    It only travels to links that are in the correct domains as given in the project description.
    It removes queries and fragments before storing and traversing to new links
    It does not store any further information besides the links it has seen as of right now

--- scraper.py
+++ scraper.py
@@ -1,11 +1,74 @@
 import re
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+ACCEPTED_DOMAINS = [re.compile(r'.*[\W]ics\.uci\.edu\/.*'), re.compile(r'.*[\W]cs\.uci\.edu\/.*'),
+                    re.compile(r'.*[\W]infomatics\.uci\.edu\/.*'), re.compile(r'.*[\W]stats\.uci\.edu\/.*'),
+                    re.compile(r'.*today\.uci\.edu\/department\/information_computer_sciences\/*')]
 
-def extract_next_links(url, resp):
+DISCOVERED_LINKS = set()
+
+TRAVERSED_COUNT = 0
+
+def scraper(url, resp):
+    global DISCOVERED_LINKS
+    global TRAVERSED_COUNT
+    links = extract_next_links(url, resp)
+
+    links_file = open('Logs/DiscoveredLinks.log', "a")
+    for link in links:
+        links_file.write(link + '\n')
+
+    links_file.close()
+
+    DISCOVERED_LINKS |= set(links)
+    test = [link for link in links if is_valid(link)]
+    #print(test)
+    TRAVERSED_COUNT += 1
+    print('Discovered Link Count: ' + str(len(DISCOVERED_LINKS)))
+    print('Number of Links Traversed: ' + str(TRAVERSED_COUNT))
+    return test
+
+def extract_next_links(url, resp): 
+    url_scheme = urlparse(url).scheme       # Getting scheme and netloc to append
+    url_netloc = urlparse(url).netloc       # in front of relative path links
+
+    if resp.status < 200 or resp.status > 399:
+        return list()
+
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+    links = [link.get('href') for link in soup.find_all('a', href=True)]
+    filtered_links = set()
+
+    for link in links: 
+
+        parsed_link = urlparse(link)
+        # First character checks
+        if re.match(r'^#.*$', link):                        # Get rid of scroll-to tags
+            continue
+        elif re.match(r'^\/[^\/].+$', link):                  # First character is / (relative path)
+            link = f'{url_scheme}://{url_netloc}{link}'     # Add the scheme and netloc to the beginning link
+        elif re.match(r'^\/\/.+$', link):
+            link = f'{url_scheme}:{link}'                   # First characters are //  (use same scheme)
+                                                            # Add scheme to the beginning of link
+
+        
+        link = link.split('?')[0]
+        link = link.split('#')[0]
+
+        if link in DISCOVERED_LINKS:
+            continue
+
+        for pattern in ACCEPTED_DOMAINS:
+            match = re.match(pattern, link)
+            if match:
+                filtered_links.add(link)
+                break
+        #filtered_links.append(link)
+
+
+    for link in filtered_links:
+        print(link)
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -15,7 +78,7 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    return list(filtered_links)
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -27,12 +90,13 @@ def is_valid(url):
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|png|tiff|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|pdf|pptx|docx|jpg|"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
 
     except TypeError:
