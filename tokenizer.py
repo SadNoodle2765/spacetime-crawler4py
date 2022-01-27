f9ea3932aa6adab0147bfa337fee31afbcc57357
Warren Wei Leu <wwleu@circinus-30.ics.uci.edu>
2022-01-26 22:15:42 -0800

    Renamed PartA.py to tokenizer.py

---
+++ tokenizer.py
@@ -0,0 +1,61 @@
+import re
+import sys
+from bs4 import BeautifulSoup
+import requests
+
+
+    
+
+
+
+def tokenize(texts):
+    tokens = []
+    pattern = re.compile("(?:[a-zA-Z0-9]+(?:-?'?[a-zA-Z0-9]+)+)|[aiAI]")
+    for token in pattern.findall(texts):
+        tokens.append(token.lower())
+    return tokens
+
+
+def computeWordFrequencies(texts):
+    stopwords = {}
+    with open('stopwords.txt', 'r') as f:
+        for line in f.readlines():
+            stopwords[line.strip()] = 1
+
+    tokenList = tokenize(texts)
+    freqencyDict = {}
+    for token in tokenList:
+        if token not in stopwords:
+            if freqencyDict.get(token) is None:
+                freqencyDict[token] = 1
+            else:
+                freqencyDict[token] += 1
+    return freqencyDict
+
+
+def printWordFreq(frequencies):
+    for token, freq in sorted(frequencies.items(), key = (lambda k: (-k[1],k[0]))):
+        print("{} - {}".format(token,freq))
+
+if __name__ == '__main__':
+
+
+    url = requests.get('https://www.ics.uci.edu/grad/admissions/Prospective_ApplicationProcess.php')
+    soup = BeautifulSoup(url.text, 'html.parser')
+    content = soup.get_text()
+
+    tokens = tokenize(content)
+    #tokens = tokenize("Hashiri Nio Testing's Mother-In-Law")
+
+    for token in tokens:
+        print(token)
+
+    wordCount = len(tokens)
+
+    print("Word Count: " + str(wordCount))
+
+   
+    #texts = soup.get_text()
+    #print(texts)
+    #printWordFreq(computeWordFrequencies(texts.strip()))
+
