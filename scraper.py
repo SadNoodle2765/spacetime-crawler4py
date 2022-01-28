fbc195961414ed23aee8378911f9587e79efeea6
Warren Wei Leu <wwleu@circinus-30.ics.uci.edu>
2022-01-27 22:28:20 -0800

    Added some more banned file extensions

--- scraper.py
+++ scraper.py
@@ -83,8 +83,8 @@ def scraper(url, resp):
 
     updateTokenDict(resp)
 
-    # DEBUG/TESTING
-    #if (TRAVERSED_COUNT >= 20):
+    #DEBUG/TESTING
+    #if (TRAVERSED_COUNT == 50):
     #    writeDataToFiles()
 
     links = extract_next_links(url, resp)
@@ -194,10 +194,10 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
         return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff|mid|mp2|mp3|mp4"
+            r".*\.(css|js|bmp|gif|jpe?g|ico|sh"
+            + r"|png|tiff|mid|mp2|mp3|mp4|java|war|sql"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|ps|eps|tex|ppt|pptx|ppsx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
